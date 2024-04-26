0f8c8cb2f091ccc7374a63ea2c6fd0711ea721ce
Rakibul Anam Rohid <rakibul.anam@brainstation-23.com>
2024-04-26 14:51:56 +0600

    Updated Project Template

--- calculator.cpp
+++ calculator.cpp
@@ -20,24 +20,22 @@ int main() {
 
     switch (op) {
         case '+':
-            result = num1 + num2;
-            cout << "Result: " << num1 << " + " << num2 << " = " << result << endl;
+            
             break;
         case '-':
-            result = num1 - num2;
-            cout << "Result: " << num1 << " - " << num2 << " = " << result << endl;
+           
             break;
         case '*':
             result = num1 * num2;
             cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
             break;
         case '/':
-            if (num2 == 0) {
-                cout << "Error: Division by zero is not allowed." << endl;
-            } else {
-                result = num1 / num2;
-                cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
-            }
+            // if (num2 == 0) {
+            //     cout << "Error: Division by zero is not allowed." << endl;
+            // } else {
+            //     result = num1 / num2;
+            //     cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
+            // }
             break;
         default:
             cout << "Error: Invalid operation." << endl;
