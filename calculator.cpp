74a6aafe3fcb44b122562fd55bfbc69a7ff63929
Rakibul Anam Rohid <rakibul.anam@brainstation-23.com>
2024-04-29 11:43:45 +0600

    Add extended fuctionality

--- calculator.cpp
+++ calculator.cpp
@@ -1,54 +1,57 @@
 #include <iostream>
 #include <cmath>
-//modification with second task
 using namespace std;
 
-int main() {
-    int num1, num2;
-    char op;
+string getParity(double num) {
+    if (fmod(num, 2) == 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
 
+string getSign(double num) {
+    if (num >= 0) {
+        return "Positive";
+    } else {
+        return "Negative";
+    }
+}
+
+int main() {
+    double num1, num2;
+    char op;
     cout << "Enter the first number: ";
     cin >> num1;
-
     cout << "Enter an operation (+, -, *, /): ";
     cin >> op;
-
     cout << "Enter the second number: ";
     cin >> num2;
 
-    int result;
-
+    double result;
     switch (op) {
         case '+':
             result = num1 + num2;
-            cout << "Result: " << num1 << " + " << num2 << " = " << result << endl;
-            if(num1>0 && num1%2==0 && num2>0 && num2%2==0 && result>0 && result%2==0){
-                cout<<"(Even ; Positive)"<<op<<"(Even ; Positive) = (Even; Positive;)\n";
-            }
+            cout << num1 << " + " << num2 << " = " << result << endl;
+            cout << "(" << getParity(num1) << "; " << getSign(num1) << ";) + (" << getParity(num2) << "; " << getSign(num2) << ";) = (" << getParity(result) << "; " << getSign(result) << ";)" << endl;
             break;
         case '-':
             result = num1 - num2;
-            cout << "Result: " << num1 << " - " << num2 << " = " << result << endl;
-            if(num1>0 && num1%2==0 && num2>0 && num2%2==0 && result>0 && result%2==0){
-                cout<<"(Even ; Positive)"<<op<<"(Even ; Positive) = (Even; Positive;)\n";
-            }
+            cout << num1 << " - " << num2 << " = " << result << endl;
+            cout << "(" << getParity(num1) << "; " << getSign(num1) << ";) - (" << getParity(num2) << "; " << getSign(num2) << ";) = (" << getParity(result) << "; " << getSign(result) << ";)" << endl;
             break;
         case '*':
             result = num1 * num2;
-            cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
-            if(num1>0 && num1%2==0 && num2>0 && num2%2==0 && result>0 && result%2==0){
-                cout<<"(Even ; Positive)"<<op<<"(Even ; Positive) = (Even; Positive;)\n";
-            }
+            cout << num1 << " * " << num2 << " = " << result << endl;
+            cout << "(" << getParity(num1) << "; " << getSign(num1) << ";) * (" << getParity(num2) << "; " << getSign(num2) << ";) = (" << getParity(result) << "; " << getSign(result) << ";)" << endl;
             break;
         case '/':
             if (num2 == 0) {
                 cout << "Error: Division by zero is not allowed." << endl;
             } else {
                 result = num1 / num2;
-                cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
-                if(num1>0 && num1%2==0 && num2>0 && num2%2==0 && result>0 && result%2==0){
-                    cout<<"(Even ; Positive)"<<op<<"(Even ; Positive) = (Even; Positive;)\n";
-                }
+                cout << num1 << " / " << num2 << " = " << result << endl;
+                cout << "(" << getParity(num1) << "; " << getSign(num1) << ";) / (" << getParity(num2) << "; " << getSign(num2) << ";) = (" << getParity(result) << "; " << getSign(result) << ";)" << endl;
             }
             break;
         default:
@@ -56,6 +59,5 @@ int main() {
             break;
     }
 
-    
     return 0;
 }
